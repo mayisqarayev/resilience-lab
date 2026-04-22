aafb594008633f951291e8f1d16d87309f2f767f
qarayevmc <mayis.qarayev@kapitalbank.az>
2026-04-22 16:04:56 +0400

    chore: initialize Spring Boot project structure

---
+++ src/test/java/com/mayis/resilience_lab/ResilienceLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mayis.resilience_lab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResilienceLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

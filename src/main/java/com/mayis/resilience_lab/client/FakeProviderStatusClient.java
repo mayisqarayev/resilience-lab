d0f72f9f6e37976533133136159102f5139df4fb
qarayevmc <mayis.qarayev@kapitalbank.az>
2026-04-22 16:49:13 +0400

    feat: add fake implementation of provider status client

--- src/main/java/com/mayis/resilience_lab/client/FakeProviderStatusClient.java
+++ src/main/java/com/mayis/resilience_lab/client/FakeProviderStatusClient.java
@@ -1,4 +1,19 @@
 package com.mayis.resilience_lab.client;
 
-public class FakeProviderStatusClient {
+import com.mayis.resilience_lab.exception.ProviderNotFoundException;
+import com.mayis.resilience_lab.model.ProviderState;
+import com.mayis.resilience_lab.model.ProviderStatus;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class FakeProviderStatusClient implements ProviderStatusClient {
+
+    public ProviderStatus getStatus(String providerId) {
+        if (providerId == null || providerId.isBlank()) {
+            throw new ProviderNotFoundException("Provider id must not be blank");
+        }
+
+        return new ProviderStatus(providerId, ProviderState.AVAILABLE, "DOWNSTREAM");
+    }
 }
